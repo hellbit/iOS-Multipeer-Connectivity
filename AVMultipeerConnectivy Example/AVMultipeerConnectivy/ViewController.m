a611d08555a78be091e39397f1245e1b3a5d1149
Aleksandr Vdovichenko <vdovichenko.a.a@gmail.com>
2015-01-22 20:57:34 +0200

    Added test project.

---
+++ AVMultipeerConnectivy Example/AVMultipeerConnectivy/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  AVMultipeerConnectivy
+//
+//  Created by Aleksandr Vdovichenko on 22.01.15.
+//  Copyright (c) 2015 Aleksander Vdovichenko. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ AVMultipeerConnectivy Example/AVMultipeerConnectivyTests/AVMultipeerConnectivyTests.m
@@ -0,0 +1,40 @@
+//
+//  AVMultipeerConnectivyTests.m
+//  AVMultipeerConnectivyTests
+//
+//  Created by Aleksandr Vdovichenko on 22.01.15.
+//  Copyright (c) 2015 Aleksander Vdovichenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface AVMultipeerConnectivyTests : XCTestCase
+
+@end
+
+@implementation AVMultipeerConnectivyTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
