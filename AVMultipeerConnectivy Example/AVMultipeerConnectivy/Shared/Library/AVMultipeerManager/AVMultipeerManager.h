7565c4539dec77ae68002ee895504ccd21de061b
Aleksandr Vdovichenko <vdovichenko.a.a@gmail.com>
2015-01-23 23:21:19 +0200

    Added manager and multipeer view controller.

---
+++ AVMultipeerConnectivy Example/AVMultipeerConnectivy/Shared/Library/AVMultipeerManager/AVMultipeerManager.h
@@ -0,0 +1,13 @@
+//
+//  AVMultipeerManager.h
+//  AVMultipeerConnectivy
+//
+//  Created by Aleksandr Vdovichenko on 23.01.15.
+//  Copyright (c) 2015 Aleksander Vdovichenko. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AVMultipeerManager : NSObject
+
+@end
